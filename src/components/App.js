ebc98ea785615113c13b4428a86fbee03bc2b7af
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-29 16:35:01 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
-
+import 'regenerator-runtime/runtime';
 const App = () => {
+  const[apiData, setApiData] = useState([]);
+  useEffect(() => {
+    const fetchData = async ()=>{
+      const response = await fetch('https://dummyjson.com/products');
+      const data = await response.json();
+      setApiData([data]);
+    }
+    fetchData();
+    }, []);
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      <pre id="output">{JSON.stringify(apiData, null, 2)}</pre>
     </div>
   )
 }
