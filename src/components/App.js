91eab3f056c4eb82564b76e55ec20bbfeea0063a
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-29 17:11:30 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,47 @@
-import React, { useEffect, useState } from "react";
-import './../styles/App.css';
-import 'regenerator-runtime/runtime';
+  import React, { useEffect, useState } from "react";
+  import './../styles/App.css';
+  import 'regenerator-runtime/runtime';
 
-const DataFetcher = () => {
-  const [apiData, setApiData] = useState([]);
-  const [error, setError] = useState(null);
+  const DataFetcher = () => {
+    const [apiData, setApiData] = useState();
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://dummyjson.com/products');
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('https://dummyjson.com/products');
+          
+          const data = await response.json();
+          if (data && data.products && data.products.length > 0) {
+            setApiData(data.products);
+          } else {
+            setApiData([]);
+          }
+        } catch (error) {
+          setError('An error occurred: ' + error.message);
+          setApiData([]);
         }
-        const data = await response.json();
-        if (data && data.products && data.products.length > 0) {
-          setApiData(data.products);
-        } else {
-          setApiData([]); // No data found
-        }
-      } catch (error) {
-        setError('An error occurred: ' + error.message);
-        setApiData([]);
-      }
-    };
-    fetchData();
-  }, []);
+      };
+      fetchData();
+    }, []);
 
-  if (error) {
-    return <h4>{error}</h4>;
-  }
+    if (error) {
+      return <h1>{'No data found'}</h1>;
+    }
 
-  if (apiData.length === 0 && !error) {
-    return <h4>No data found</h4>;
-  }
 
-  return (
-    <div id="main">
-      <h1 id="header">Data Fetched from API</h1>
-      <pre id="output">{JSON.stringify(apiData, null, 2)}</pre>
-    </div>
-  );
-};
+    return (
+      <div id="main">
+        {apiData?  (
+          <>
+            <h1 id="header">Data Fetched from API</h1>
+            <pre id="output">{JSON.stringify(apiData, null, 2)|| apiData}</pre>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    );
+  };
 
-export default DataFetcher;
+  export default DataFetcher;
